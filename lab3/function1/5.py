be3598002addf8b405e8d5073b6f191f11f9229d
Kadyrzhan <rfmsh16@gmail.com>
2025-06-10 08:17:28 +0500

    lab3 commit

---
+++ lab3/function1/5.py
@@ -0,0 +1,7 @@
+from 9 import is_prime(numbers)
+from 12 import has_33(somelist)
+from 13 import spy_game(lst)
+
+print(is_prime("1 6 8 55 4 3556 2 7 3 11 57 9"))
+print(has_33([1,2,3,4,5,6,4,3,3,3,2,1]))
+print(spy_game([6,4,3,7,9,6,3,0,9,5,0,6,4,2,7]))
